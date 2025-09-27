3003818b0fba95576d691633bec2d5d8f5e5ba73
L.F. Pemi Magar <lfpemimagar@gmail.com>
2025-09-27 07:39:51 +0545

    scrapped all details from a static link

--- scrap.py
+++ scrap.py
@@ -1,6 +1,16 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-resp = requests.get("https://www.linkedin.com/jobs/search?keywords=Frontend%20Developer&location=Nepal")
-soup = bs(resp.content)
-print(soup.text)
+resp = requests.get(
+    "https://www.linkedin.com/jobs/search?keywords=Frontend%20Developer&location=Nepal"
+)
+soup = bs(resp.content,"html.parser")
+
+items = soup.find_all("div", class_="base-card")
+for item in items:
+    link = item.find("a")["href"].strip()
+    title = item.find("h3", class_="base-search-card__title").text.strip().replace(",","")
+    company = item.find("a", class_="hidden-nested-link")["href"].strip().replace(",","")
+    company_link = item.find("a", class_="hidden-nested-link").text.strip()
+    location = item.find("span",class_="job-search-card__location").text.strip().replace(",","")
+    print(f"{title},{company},{location},{company_link},{link}\n")
